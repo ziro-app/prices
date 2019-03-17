ed8291d0ad618396e6a24127fc8cf45d4c0c6c4b
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-03-16 19:08:36 -0700

    first commit, basic config

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const CompressionPlugin = require('compression-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = (env, { mode }) => {
+	const config = {
+		module: {
+			rules: [
+				{
+					test: /\.js$/, 
+					exclude: /node_modules/, 
+					use: {
+						loader: 'babel-loader',
+						options: {
+							presets: [
+								'@babel/preset-env',
+								'@babel/preset-react'
+							],
+							plugins: [
+								'@babel/plugin-transform-runtime',
+								'@babel/plugin-proposal-class-properties'
+							]
+						}
+					}
+				},
+				{
+					test: /\.css$/,
+					use: ['style-loader', 'raw-loader']
+				}
+			]
+		},
+		plugins: [ new HtmlWebpackPlugin({ template: './src/index.html' }) ]
+	}
+	if (mode === 'development') {
+		config.devtool = 'cheap-module-eval-source-map'
+	}
+	if (mode === 'production') {
+		config.devtool = 'cheap-module-source-map'
+		config.plugins.push(
+			new CompressionPlugin()
+		)
+	}
+	return config
+}

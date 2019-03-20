15dfa5e2d5d61927caa4af65aaa12965dded689f
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-03-20 09:53:48 -0700

    add component to display ui message if there is an error fetching initial data

---
+++ src/ErrorOnFetch/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import AlertSvg from './AlertSvg'
+import { container, errorMessage } from './styles'
+
+const ErrorOnFetch = () =>
+	<div style={container}>
+		<AlertSvg size={32} />
+		<p style={errorMessage}>Ocorreu um erro no carregamento dos dados</p>
+		<p style={errorMessage}>Teste sua conexão à internet e tente novamente</p>
+		<p style={errorMessage}>Se o erro persistir contate o suporte</p>
+	</div>
+
+export default ErrorOnFetch
---
+++ src/ErrorOnFetch/styles.js
@@ -0,0 +1,11 @@
+export const
+
+container = {
+	textAlign: 'center',
+	color: '#F16B6F'
+},
+
+errorMessage = {
+	fontSize: '1.6rem',
+	fontWeight: '700'
+}

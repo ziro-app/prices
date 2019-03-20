25e33add88fd81b6bbf264a3550e5ddef9db72d8
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-03-20 09:49:54 -0700

    add function to render form

--- src/PriceCreate/index.js
+++ src/PriceCreate/index.js
@@ -6,7 +6,7 @@ import { initialUiState, changeUiState } from './methods/stateMachine'
 import fetchInitialData from './methods/fetchInitialData'
 import updateDropdown from './methods/updateDropdown'
 // import submitForm from './methods/submitForm'
-// import renderForm from './methods/renderForm'
+import renderForm from './methods/renderForm'
 
 export default class PriceCreate extends Component {
 	state = {
@@ -25,7 +25,7 @@ export default class PriceCreate extends Component {
 	fetchInitialData = fetchInitialData(this)
 	updateDropdown = updateDropdown(this)
 	// submitForm = submitForm(this)
-	// renderForm = renderForm(this)
+	renderForm = renderForm(this)
 	/*-- lifecycle --*/
 	componentDidMount = () => this.fetchInitialData()
 	componentWillUnmount = () => this.cancelTokenSource.cancel()
---
+++ src/PriceCreate/methods/renderForm.js
@@ -0,0 +1,23 @@
+import React from 'react'
+// import Form from '../Form/index'
+import Header from '../../Header/index'
+// import Spinner from '../../Spinner/index'
+// import ErrorOnFetch from '../../ErrorOnFetch/index'
+
+const renderForm = that => uiState => {
+	const componentToRender = {
+		default:
+			<Form
+				state={that.state}
+				updateDropdown={that.updateDropdown}
+			/>,
+		fetching:
+			<Spinner size={'8rem'} />,
+		error_fetching:
+			<ErrorOnFetch />
+	}
+	const ui = uiState !== 'fetching' && uiState !== 'error_fetching' ? 'default' : uiState
+	return <Header title='Cadastrar retirada'>{componentToRender[ui]}</Header>
+}
+
+export default renderForm

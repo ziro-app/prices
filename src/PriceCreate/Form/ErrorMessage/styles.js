0738c1aecbacb14c9babc9b4695f1b90122b8c70
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-03-20 10:08:49 -0700

    add ErrorMessage component

---
+++ src/PriceCreate/Form/ErrorMessage/styles.js
@@ -0,0 +1,11 @@
+export const
+
+error = {
+	display: 'grid',
+	gridTemplateColumns: '16px 1fr',
+	alignItems: 'center',
+	marginBottom: '6px',
+	fontSize: '1.5rem',
+	fontWeight: '700',
+	color: '#F16B6F'
+}

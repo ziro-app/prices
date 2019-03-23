0e8d0e593dd1cfe8ac80ced982d44ee4e9e0d163
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-03-23 08:27:23 -0700

    update initial data spreadsheet

--- src/PriceCreate/utils/fetchFromSheet.js
+++ src/PriceCreate/utils/fetchFromSheet.js
@@ -7,7 +7,7 @@ const fetchFromSheet = async (get, cancelTokenSource) => {
 		await Promise.reject('Error at fetchFromSheet. values is undefined')
 	if (values.length === 0)
 		await Promise.reject('Error at fetchFromSheet. values.length === 0')
-	const suppliers = values.map(value => value[5]).slice(2).filter(value => Boolean(value[0]))
+	const suppliers = values.map(value => value[3]).slice(1).filter(value => Boolean(value[0]))
 	return { suppliers }
 }
 

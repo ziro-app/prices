6a77379defb871487979852288b9bc96c9c40611
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-03-23 14:32:53 -0700

    final adjustments to products array

--- src/PriceCreate/utils/getOptions.js
+++ src/PriceCreate/utils/getOptions.js
@@ -2,20 +2,18 @@ export const
 
 getProducts = (data, index) => {
 	const raw = data.map(value => value.slice(0,index)).slice(1).filter(value => Boolean(value[0]))
-	const step = raw.map(value => [ ...value, Math.round((value[2] - value[1]) / 19) ])
-	const page = step.map((value, index) => [ ...value, index !== step.length - 1 ? 'middle' : 'last' ])
-	console.log(page)
-	return raw
+	const step = raw.map(value => [...value, Math.round((value[2] - value[1]) / 19)]).sort()
+	const page = step.map((value, index) => [...value, index !== step.length - 1 ? 'middle' : 'last'])
+	const minMax = [...page.map(value => [...value, 'min']), ...page.map(value => [...value, 'max'])].sort()
+	return minMax.map(value => ({
+		name: value[0],
+		type: value[5],
+		page: value[4],
+		min: value[1],
+		max: value[2],
+		step: value[3]
+	}))
 },
 
 getSuppliers = (data, index) =>
 	data.map(value => value[index]).slice(1).filter(value => Boolean(value[0]))
-
-/*
-	name: 'bijuteria',
-	type: 'min',
-	page: 'middle',
-	min: '19.90',
-	max: '199.90',
-	step: '30.90'
-*/

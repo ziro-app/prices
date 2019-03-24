752fbac5c31346f3004736d1699e98f2315d2d1a
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-03-24 08:24:47 -0700

    fix products array

--- src/PriceCreate/utils/getOptions.js
+++ src/PriceCreate/utils/getOptions.js
@@ -3,22 +3,22 @@ export const
 getProducts = (data, index) => {
 	const raw = data.map(value => value.slice(0,index)).slice(1).filter(value => Boolean(value[0]))
 	const step = raw.map(value => [...value, Math.round((value[2] - value[1]) / 19)]).sort()
-	const next = step.map((value, index) => {
-		if (step[index + 1])
-			return [...value, step[index + 1][0]]
+	const minMax = [...step.map(value => [...value, 'min']), ...step.map(value => [...value, 'max'])].sort()
+	const next = minMax.map((value, index) => {
+		if (minMax[index + 1])
+			return [...value, `${minMax[index + 1][0]}-${minMax[index + 1][4]}`]
 		return [...value, '']
 	})
 	const prev = next.map((value, index) => {
-		if (step[index - 1])
-			return [...value, step[index - 1][0]]
+		if (next[index - 1])
+			return [...value, `${next[index - 1][0]}-${next[index - 1][4]}`]
 		return [...value, '']
 	})
-	const minMax = [...prev.map(value => [...value, 'min']), ...prev.map(value => [...value, 'max'])].sort()
-	return minMax.map(value => ({
+	return prev.map(value => ({
 		name: value[0],
-		type: value[6],
-		prev: value[5],
-		next: value[4],
+		type: value[4],
+		prev: value[6],
+		next: value[5],
 		min: value[1],
 		max: value[2],
 		step: value[3]

2da1dea0ee22d3924c5e30d15add488de031f41e
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-03-26 19:42:53 -0700

    render options dynamically

--- src/PriceCreate/Form/Questions/index.js
+++ src/PriceCreate/Form/Questions/index.js
@@ -7,26 +7,35 @@ import Dropdown from '@ziro/dropdown'
 import { input } from './styles'
 
 const Questions = ({ state, updateDropdown, params, url }) => {
-	const { uiState, suppliers, supplier, error_supplier, products } = state
+	const { uiState, suppliers, supplier, error_supplier, products, pageIndex } = state
+	if (url === '/')
+		return (
+			<InputWrapper uiState={uiState} errorMessage={error_supplier}
+				render={() => (
+					<Dropdown
+						name='supplier'
+						placeholder='Fabricante'
+						options={suppliers}
+						value={supplier}
+						updateParent={updateDropdown}
+					/>
+				)}
+				renderSubmitting={() => (
+					<input
+						style={input}
+						placeholder={supplier}
+						disabled={true}
+					/>
+				)}
+			/>
+		)
 	return (
-		<InputWrapper uiState={uiState} errorMessage={error_supplier}
-			render={() => (
-				<Dropdown
-					name='supplier'
-					placeholder='Fabricante'
-					options={suppliers}
-					value={supplier}
-					updateParent={updateDropdown}
-				/>
-			)}
-			renderSubmitting={() => (
-				<input
-					style={input}
-					placeholder={supplier}
-					disabled={true}
-				/>
-			)}
-		/>
+		products[pageIndex].options.map(option =>
+			<div key={option}>
+				<input type='radio' id={option} name={products[pageIndex].name} value={option} />
+				<label htmlFor={option}>{option}</label>
+			</div>
+		)
 	)
 }
 

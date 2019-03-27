ac3c700104323774353e5e98a3927e93dbca1404
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-03-26 20:47:28 -0700

    generate array of options

--- src/PriceCreate/utils/getOptions.js
+++ src/PriceCreate/utils/getOptions.js
@@ -2,8 +2,17 @@ export const
 
 getProducts = (data, index) => {
 	const raw = data.map(value => value.slice(0,index)).slice(1).filter(value => Boolean(value[0]))
-	const step = raw.map(value => [...value, Math.round((value[2] - value[1]) / 19)]).sort()
-	const minMax = [...step.map(value => [...value, 'min']), ...step.map(value => [...value, 'max'])].sort()
+	const options = raw.map(value => {
+		const min = parseFloat(value[1])
+		const max = parseFloat(value[2])
+		const step = Math.round((max - min) / 18)
+		let optionsArray = []
+		for (let i = 0; i < 18; i ++)
+			optionsArray.push(min + i * step)
+		optionsArray.push(max)
+		return [...value, optionsArray]
+	}).sort()
+	const minMax = [...options.map(value => [...value, 'min']), ...options.map(value => [...value, 'max'])].sort()
 	const next = minMax.map((value, index) => {
 		if (minMax[index + 1])
 			return [...value, `${minMax[index + 1][0]}-${minMax[index + 1][4]}`]
@@ -19,18 +28,14 @@ getProducts = (data, index) => {
 		type: value[4],
 		prev: value[6],
 		next: value[5],
-		min: value[1],
-		max: value[2],
-		step: value[3]
+		options: value[3]
 	}))
 	const supplier = {
 		name: 'fabricante',
 		type: '',
 		prev: '',
 		next: `${products[0].name}-${products[0].type}`,
-		min: '',
-		max: '',
-		step: ''
+		options: ''
 	}
 	return [supplier, ...products]
 },

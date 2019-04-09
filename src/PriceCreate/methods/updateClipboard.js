25753dc55e4567f4daf6f9d4e7c0781c65056d8a
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-04-08 17:40:08 -0700

    write to clipboard

--- src/PriceCreate/methods/updateClipboard.js
+++ src/PriceCreate/methods/updateClipboard.js
@@ -1,5 +1,7 @@
-const updateClipboard = that => () => {
-
+const updateClipboard = that => async url => {
+	console.log(url)
+	const result = await navigator.clipboard.writeText('Ziro Clipboard')
+	console.log(result)
 }
 
 export default updateClipboard

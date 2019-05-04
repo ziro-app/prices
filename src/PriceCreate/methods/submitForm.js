43f000209f3603569512763efffa9ed1b2aa109e
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-05-03 20:17:20 -0700

    prevent submission without all field being filled

--- src/PriceCreate/methods/submitForm.js
+++ src/PriceCreate/methods/submitForm.js
@@ -1,26 +1,34 @@
 // import sendToBackend from '../utils/sendToBackend'
 
 const submitForm = that => async () => {
-	try {
-		that.changeUiState('SUBMIT')
-		// await sendToBackend(that.state, sale, action)
-		await Promise.resolve('ok')
-		that.changeUiState('SUCCESS')
-		alert('Enviado com sucesso!')
-		that.props.history.push('/')
-		const toUpdate = { pageIndex: 0 }
-		const keys = Object.keys(that.state)
-		for (let i = 0; i < keys.length; i++) {
-			if (!keys[i].match(/pageIndex|uiState|products|suppliers/g))
-				toUpdate[keys[i]] = ''
+	const keys = Object.keys(that.state)
+	const formIsValid = keys.filter(value => {
+		if (value.match(/pageIndex|uiState|products|suppliers|supplier|search|error_supplier|error_price/g))
+			return false
+		return !that.state[value]
+	}).length === 0
+	if (formIsValid) {
+		try {
+			that.changeUiState('SUBMIT')
+			// await sendToBackend(that.state, sale, action)
+			await Promise.resolve('ok')
+			that.changeUiState('SUCCESS')
+			alert('Enviado com sucesso!')
+			that.props.history.push('/')
+			const toUpdate = { pageIndex: 0 }
+			for (let i = 0; i < keys.length; i++) {
+				if (!keys[i].match(/pageIndex|uiState|products|suppliers/g))
+					toUpdate[keys[i]] = ''
+			}
+			that.setState(toUpdate)
+		} catch (error) {
+			console.log(error)
+			that.changeUiState('ERROR')
+			if (error.response)
+				console.log(error.response)
 		}
-		that.setState(toUpdate)
-	} catch (error) {
-		console.log(error)
-		that.changeUiState('ERROR')
-		if (error.response)
-			console.log(error.response)
-	}
+	} else
+		alert('Erro. Perguntas em branco.')
 }
 
 export default submitForm

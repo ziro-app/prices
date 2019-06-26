6d5c7145283ede2e52e7a04007369b1b195e72b6
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-06-26 14:25:04 -0300

    replace ',' for '.'

--- src/PriceCreate/utils/sendToBackend.js
+++ src/PriceCreate/utils/sendToBackend.js
@@ -2,6 +2,7 @@ import { post } from 'axios'
 
 const sendToBackend = async data => {
 	const { uiState, pageIndex, search, products, suppliers, error_supplier, error_price, ...dataToSubmit } = data
+	Object.keys(dataToSubmit).map(key => dataToSubmit[key] = dataToSubmit[key].replace(',','.'))
 	console.log(dataToSubmit)
 	const { data: message } = await post(`${process.env.API_URL}`, dataToSubmit)
 	if (message !== 'Success')

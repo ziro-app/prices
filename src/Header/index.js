1d750dfc3170c08d6a587205741f55da5cf1aa17
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-06-26 10:15:11 -0300

    update logo

--- src/Header/index.js
+++ src/Header/index.js
@@ -8,8 +8,8 @@ const Header = ({ title, subtitle, children }) =>
 	    <Image
 			cloudName='ziro'
 			width='35'
-			publicId='logo-round_kxn8sa'
-			version='1508000699'
+			publicId='logo-app_fwothv'
+			version='1561160634'
 			format='png'
 			secure='true'
 			alt='logo'
